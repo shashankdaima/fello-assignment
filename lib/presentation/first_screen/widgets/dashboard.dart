9911406a5a24094a43195c4733b65ecf4745d9dc
Shashank Daima <daimashashank10@gmail.com>
2023-12-09 19:31:13 +0530

    minor

--- lib/presentation/first_screen/widgets/dashboard.dart
+++ lib/presentation/first_screen/widgets/dashboard.dart
@@ -1,18 +1,39 @@
+import 'dart:ffi' as ffi;
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rive/rive.dart' as rive;
+import 'package:rive/rive.dart';
 
-class Dashboard extends ConsumerWidget {
+class Dashboard extends ConsumerStatefulWidget {
   const Dashboard({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _DashboardState();
+}
+
+class _DashboardState extends ConsumerState<Dashboard> {
+  SMITrigger? input;
+
+  void _onRiveInit(Artboard artboard) {
+    final controller = StateMachineController.fromArtboard(artboard, 'input');
+    artboard.addController(controller!);
+    input = controller.findInput<ffi.Float>('input') as SMITrigger;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final Size screenDimens = MediaQuery.of(context).size;
     return Container(
         width: screenDimens.width,
         height: screenDimens.width / 1.2,
         padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
-        child: const rive.RiveAnimation.asset("assets/tree_anim.riv"));
+        child: rive.RiveAnimation.asset(
+          "assets/tree_anim.riv",
+          onInit: (artboard) {
+            _onRiveInit(artboard);
+          },
+        ));
   }
 }
