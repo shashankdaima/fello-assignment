35b5b7c43a15799b9f1072b8a04cf2266ed77dc2
Shashank Daima <daimashashank10@gmail.com>
2023-12-09 17:04:06 +0530

    drawer image working, checkpoint

--- lib/presentation/common_ui/drawer_layout.dart
+++ lib/presentation/common_ui/drawer_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:flutter_template/presentation/common_ui/tree_list_element.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,40 +10,40 @@ class DrawerLayout extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Drawer(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: ListView.builder(itemBuilder: (BuildContext ctx, int idx) {
-          if (idx == 0) {
-            return const DrawerHeader(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/india-gate.jpg'),
-                  fit: BoxFit.cover,
-                ),
+      child: ListView.builder(itemBuilder: (BuildContext ctx, int idx) {
+        if (idx == 0) {
+          return DrawerHeader(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.pink, Color(0xFF4D4C61)],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
               ),
-              child: null, // You can add additional content if needed
-            );
-          } else if (idx == 1) {
-            return Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: FloatingActionButton.extended(
-                onPressed: () {
-                  // Handle the button press
-                  print('Add New Goal pressed');
-                },
-                icon: const Icon(Icons.add),
-                label: Text(
-                  'Add New Goal',
-                  style: GoogleFonts.robotoCondensed(
-                      fontSize: 18.0, fontWeight: FontWeight.w500),
-                ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [Image.asset("assets/reward_png.png")],
+            ),
+          );
+        } else if (idx == 1) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 12),
+            child: FloatingActionButton.extended(
+              onPressed: () {
+                // Handle the button press
+                print('Add New Goal pressed');
+              },
+              icon: const Icon(Icons.add),
+              label: Text(
+                'Add New Goal',
+                style: GoogleFonts.robotoCondensed(
+                    fontSize: 18.0, fontWeight: FontWeight.w500),
               ),
-            );
-          }
-          return const TreeListElement(
-              title: "Element", progress: 12, stage: 6);
-        }),
-      ),
+            ),
+          );
+        }
+        return const TreeListElement(title: "Element", progress: 12, stage: 6);
+      }),
     );
   }
 }
